aebcf611f05214b3cd528a6d4a5968876dc6aae4
Golubeff-RA <83964300+Golubeff-RA@users.noreply.github.com>
2025-03-03 12:21:30 +0300

    solved power

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,5 +2,9 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+    if (x < 0 && n % 2 == 1) {
+        return -power(-x, n);
+    }
+
+    return n == 0 ? 1 : n % 2 == 0 ? power(x, n / 2) * power(x, n / 2) : power(x, n - 1) * x;
 }
