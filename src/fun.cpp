8e641c88fd18261980b2a6bfb10c815cb412748d
Golubeff-RA <83964300+Golubeff-RA@users.noreply.github.com>
2025-03-03 15:08:19 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,9 +2,5 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    if (x < 0 && n % 2 == 1) {
-        return -power(-x, n);
-    }
-
     return n == 0 ? 1 : n % 2 == 0 ? power(x, n / 2) * power(x, n / 2) : power(x, n - 1) * x;
 }
